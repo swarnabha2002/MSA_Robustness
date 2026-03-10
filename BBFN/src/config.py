643b746be12630b5203a272685b4d5ce85dd7713
swarnabha2002 <70818511+swarnabha2002@users.noreply.github.com>
2026-03-10 11:08:43 +0530

    Update config.py

--- BBFN/src/config.py
+++ BBFN/src/config.py
@@ -9,7 +9,7 @@ from torch import optim
 import torch.nn as nn
 
 # path to a pretrained word embedding file
-word_emb_path = '/home/yingting/Glove/glove.840B.300d.txt'
+word_emb_path = '/content/MSA_Robustness/BBFN/CMU-MultimodalSDK/embeddings/glove.840B.300d.txt'
 assert(word_emb_path is not None)
 
 
